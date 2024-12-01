8986bac69538822c16c727048f685c77609a2a66
I Made Sugi Ardana <sugiardana@gmail.com>
2024-12-01 16:19:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit as st
 from datetime import datetime
 
 # Panca Wara
-arr_pwara = ['Umanis', 'Pahing', 'Pon', 'Wage', 'Kliwon']
+arr_pwara = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Umanis']
+
+# Sampta Wara
+arr_stwara = ['Soma', 'Anggara', 'Buda', 'Wrespati', 'Sukra', 'Saniscara', 'Redite']
 
 # Judul aplikasi
 st.title("Hitung Panca Wara Berdasarkan Tanggal")
@@ -25,10 +28,11 @@ if tanggal_input:
         # Hitung selisih hari
         delta = (input_date - start_date).days
         pwara = delta % 5
+        stwara = delta % 7
 
         # Tampilkan hasil
         st.success(f"Jumlah hari dari 01/01/1900 sampai {tanggal_input.strftime('%d/%m/%Y')}: {delta} hari")
-        st.info(f"Panca Wara: **{arr_pwara[pwara]}**")
+        st.info(f"Wewaran: **{arr_pwara[pwara]} {arr_stwara[stwara]}**")
     except Exception as e:
         # Jika terjadi kesalahan
         st.error(f"Terjadi kesalahan: {e}")
